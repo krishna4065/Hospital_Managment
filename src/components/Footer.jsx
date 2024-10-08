eab6539806b3455ccbe8bfe635e0b007c6266b53
vedant <167603512+krishna4065@users.noreply.github.com>
2024-10-08 13:46:05 +0530

    footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -72,11 +72,11 @@ const Footer = () => {
             </div>
             <div>
               <MdEmail />
-              <span>zeelab@gmail.com</span>
+              <span>vedantvardhantripathi@gmail.com</span>
             </div>
             <div>
               <FaLocationArrow />
-              <span>Karachi, Pakistan</span>
+              <span>Uttar Pradesh, India</span>
             </div>
           </div>
         </div>
